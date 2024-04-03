055534a2e9ef566beb6b321bef29aafaa5f4095a
rafa.cavalinhos <202002107@estudantes.ips.pt>
2024-04-03 09:35:00 +0100

    Nivel 3 - AirTranportation + Correção de erros

--- AirTransportation.java
+++ AirTransportation.java
@@ -9,10 +9,12 @@ public class AirTransportation extends Transport
 {
     private String name;
     private int numberOfContainers;
-    private static final int honoraryPercentage = 4;
+    private static final double HONORARY_PERCENTAGE = 4.0;
     
     public AirTransportation(String name, int numberOfContainers)
     {
+        super();
+        super.setFees(HONORARY_PERCENTAGE);
         this.name = name;
         this.numberOfContainers = numberOfContainers;
     } 
@@ -27,6 +29,18 @@ public class AirTransportation extends Transport
     
     public void setNumberOfContainers(int numberOfContainers) {
         this.numberOfContainers = numberOfContainers;
+    }
+    
+    @Override
+    public double getPriceWithFees() {
+        return super.getPrice() * (1.0 + (super.getFees() * 0.01));
+    }
+    
+    public String getTransportType() {return "Air Transportation";}
+    
+    @Override
+    public String toString() {
+        return super.toString() + "Transporte: " + this.name + "\nNúmero de Contentores: " + this.numberOfContainers;
     }
     
 }
